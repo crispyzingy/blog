f2fb69b1f2a96384dfaa2e65c79e5fbedad905f7
Arbaaz Khan <crispyzingy@gmail.com>
2020-09-19 14:54:16 +0530

    updated db config

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,8 +1,8 @@
 <?php
 
 // connect to database
-// $conn = mysqli_connect('remotemysql.com', 'sBn4EmvzvQ', 'U0jDwcxFCZ', 'sBn4EmvzvQ');
-$conn = mysqli_connect('localhost', 'arbaaz', 'test1234', 'blog');
+$conn = mysqli_connect('remotemysql.com', 'sBn4EmvzvQ', 'U0jDwcxFCZ', 'sBn4EmvzvQ');
+// $conn = mysqli_connect('localhost', 'arbaaz', 'test1234', 'blog');
 
 // check connection
 if(!$conn){
